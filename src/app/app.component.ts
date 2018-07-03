4581e3bb3ff6b7fac1fc83d7136cd7a78520551a
chunkingz <fortuneking33@yahoo.com>
2018-07-04 00:33:34 +0100

    Updated to read and display data properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/observable';
-import { Ibusiness } from './interfaces/ibusiness';
-import { Icategory } from './interfaces/icategory';
 import {Injectable} from '@angular/core';
 
 @Injectable()
@@ -15,13 +12,13 @@ import {Injectable} from '@angular/core';
 })
 export class AppComponent implements OnInit {
   businesses: any[];
-  businessKeys: any;
+  // businessKeys: any;
   categories: any[];
-  categoryKeys: any;
+  // categoryKeys: any;
   loader: boolean;
   activeKey: any;
   appState: any;
-  some: any;
+  // objectKeys = Object.keys;
 
   constructor(private fb: AngularFirestore) {
    console.log('constructor ran');
@@ -35,9 +32,10 @@ export class AppComponent implements OnInit {
       this.fb.collection('/businesses').valueChanges()
       .subscribe(items => {
         this.businesses = items;
-        this.businessKeys = Object.keys(this.businesses);
+        // this.businessKeys = Object.keys(this.businesses);
       console.log(this.businesses);
-      console.log(this.businessKeys);
+      // console.log(this.businessKeys);
+      // console.log(this.businessKeys.length);
       this.loader = false;
       }, error => {
           console.log(error);
@@ -47,9 +45,10 @@ export class AppComponent implements OnInit {
       this.fb.collection('/categories').valueChanges()
       .subscribe(items => {
         this.categories = items;
-        this.categoryKeys = Object.keys(this.categories);
+        // this.categoryKeys = Object.keys(this.categories);
       console.log(this.categories);
-      console.log('keys: ' + this.categoryKeys);
+      // console.log(this.categoryKeys);
+      // console.log(this.categoryKeys.length);
       this.loader = false;
       }, error => {
           console.log(error);
@@ -58,30 +57,14 @@ export class AppComponent implements OnInit {
 
    }
 
-
-    /* getBusinesses() {
-      this.fb.collection('/businesses').valueChanges()
-      .subscribe(items => {
-        this.businesses = items;
-      console.log(this.businesses);
-      });
-      // return this.businesses;
-    } */
-
-    /* getCategories() {
-      this.fb.collection('/categories').valueChanges()
-      .subscribe(items => {
-        this.categories = items;
-      console.log(this.categories);
-      });
-      // return this.categories;
-    } */
-
     changeState(state, key) {
-      if (key) {
+      if (key >= 0) {
         this.activeKey = key;
       }
       this.appState = state;
+      console.log('key: ' + key);
+      console.log('state: ' + state);
+
     }
 
 }
